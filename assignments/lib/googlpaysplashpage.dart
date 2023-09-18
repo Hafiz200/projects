8dbeabdcd1d11098ea985583240d126d5173b9cc
Hafiz200 <84306153+Hafiz200@users.noreply.github.com>
2023-09-18 09:31:50 +0530

    first commit

---
+++ assignments/lib/googlpaysplashpage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: splashpage(),
+  ));
+}
+
+class splashpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(image: AssetImage("assets/icons/google-pay.png"),width:100,height: 100,),
+            Column(
+              mainAxisAlignment:MainAxisAlignment.end,
+              children: [
+                Text("Google"),
+              ],
+            )]
+        ),
+      ),
+    );
+  }
+}
